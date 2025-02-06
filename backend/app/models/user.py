03638a80dfd18ae7677ef25760937136c774fa79
kartikey1188 <kartikeysingh428@gmail.com>
2025-02-07 05:13:07 +0530

    Student API endpoints added

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,15 +1,24 @@
 from enum import Enum
 from . import db, bcrypt
 
-# class CourseLevel(Enum):
-#     FOUNDATIONAL = 'Foundational'
-#     DIPLOMA = 'Diploma'
-#     DEGREE = 'Degree'
+class CourseLevel(Enum):
+    FOUNDATIONAL = 'Foundational'
+    DIPLOMA = 'Diploma'
+    DEGREE = 'Degree'
 
-# class CourseType(Enum):
-#     DATA_SCIENCE = 'Data Science'
-#     PROGRAMMING = 'Programming'
-#     MISCELLANEOUS = 'Miscellaneous'
+class CourseType(Enum):
+    DATA_SCIENCE = 'Data Science'
+    PROGRAMMING = 'Programming'
+    MISCELLANEOUS = 'Miscellaneous'
+
+class GradeObtained(Enum):
+    NA = 'NA'  
+    S = 'S'    
+    A = 'A'    
+    B = 'B'    
+    C = 'C'    
+    D = 'D'    
+    E = 'E'    
     
 class Role(Enum):
     STUDENT = 'student'
@@ -33,11 +42,11 @@ class User(db.Model):
         return bcrypt.check_password_hash(self.password_hash, password)
 
 
-# class Student(db.Model):
-#     __tablename__ = 'student'
-#     id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
-#     user = db.relationship('User', back_populates='student')
-#     courses = db.relationship('Course', secondary='student_courses', back_populates='students', cascade="all, delete-orphan")
+class Student(db.Model):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
+    user = db.relationship('User', back_populates='student')
+    courses = db.relationship('Course', secondary='student_courses', back_populates='students', cascade="all, delete-orphan")
 
 # class Instructor(db.Model):
 #     __tablename__ = 'instructor'
@@ -46,18 +55,18 @@ class User(db.Model):
 #     courses = db.relationship('Course', secondary='instructor_courses', back_populates='instructors') # the courses that this particular instructor teaches
 #     description = db.Column(db.Text) # a description of this instructor, things like educational background or achievements 
 
-# class Course(db.Model): 
-#     __tablename__ = 'course'
-#     course_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     course_name = db.Column(db.String(300), nullable=False, unique=True) # for example : Software Engineering
-#     course_code = db.Column(db.String(50), nullable=False, unique=True) # for example : BSCCS3003
-#     description = db.Column(db.Text) # a brief description of the course
-#     level = db.Column(db.Enum(CourseLevel), nullable=False) # level can be 'Foundational' or 'Diploma' or 'Degree'
-#     type = db.Column(db.Enum(CourseType), nullable=False) # type can be either 'Data Science' or 'Programming' or 'Miscellaneous' 
-#     image = db.Column(db.String(300)) # whatever image we'd want to associate with the course
-#     instructors = db.relationship('Instructor', secondary='instructor_courses', back_populates='courses') # the instructors teaching this course
-#     students = db.relationship('Student', secondary='student_courses', back_populates='courses') # the students enrolled in this course
-#     assignments = db.relationship('Assignment', back_populates='course', cascade="all, delete-orphan") 
+class Course(db.Model): 
+    __tablename__ = 'course'
+    course_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    course_name = db.Column(db.String(300), nullable=False, unique=True) # for example : Software Engineering
+    course_code = db.Column(db.String(50), nullable=False, unique=True) # for example : BSCCS3003
+    description = db.Column(db.Text) # a brief description of the course
+    level = db.Column(db.Enum(CourseLevel), nullable=False) # level can be 'Foundational' or 'Diploma' or 'Degree'
+    type = db.Column(db.Enum(CourseType), nullable=False) # type can be either 'Data Science' or 'Programming' or 'Miscellaneous' 
+    image = db.Column(db.String(300)) # whatever image we'd want to associate with the course
+    instructors = db.relationship('Instructor', secondary='instructor_courses', back_populates='courses') # the instructors teaching this course
+    students = db.relationship('Student', secondary='student_courses', back_populates='courses') # the students enrolled in this course
+    assignments = db.relationship('Assignment', back_populates='course', cascade="all, delete-orphan") 
 
 # class Assignment(db.Model):  # Represents an assignment associated with a course.
 #     __tablename__ = 'assignment'
@@ -73,9 +82,8 @@ class User(db.Model):
 #     instructor_id = db.Column(db.Integer, db.ForeignKey('instructor.id', ondelete="CASCADE"), primary_key=True)
 #     course_id = db.Column(db.Integer, db.ForeignKey('course.course_id', ondelete="CASCADE"), primary_key=True)
 
-# class StudentCourses(db.Model): # Implements a many-to-many relationship between Student and Course.
-#     __tablename__ = 'student_courses'
-#     student_id = db.Column(db.Integer, db.ForeignKey('student.id', ondelete="CASCADE"), primary_key=True)
-#     course_id = db.Column(db.Integer, db.ForeignKey('course.course_id', ondelete="CASCADE"), primary_key=True)
-#     grade_obtained = db.Column(db.String(50), nullable=False, default='NA') # this is the grade obtained by a particular student in a particular course - grade_obtained by default is NA, but can be changed to S,A,B,C and so on (after the result)
-#     # can make Enum for grade_obtained as well, haven't done that here at the moment for brevity's sake
+class StudentCourses(db.Model): # Implements a many-to-many relationship between Student and Course.
+    __tablename__ = 'student_courses'
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id', ondelete="CASCADE"), primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id', ondelete="CASCADE"), primary_key=True)
+    grade_obtained = db.Column(db.Enum(GradeObtained), nullable=False, default='NA') # this is the grade obtained by a particular student in a particular course - grade_obtained by default is NA, but can be changed to S,A,B,C and so on (after the result)
