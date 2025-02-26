9c46c0be43dac4785511b1264d6501ffbf9799a4
Surya Vikram <suryavikramrajput@gmail.com>
2025-02-26 15:56:28 +0530

    add python week 1 questions

--- backend/app/apis/__init__.py
+++ backend/app/apis/__init__.py
@@ -7,6 +7,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.
 jwt = JWTManager()
 api = Api()
 
-from . import auth, admin, student, instructor, course, all_marshals, lecture,lecture_feedback
+from . import auth, admin, student, instructor, course, all_marshals, lecture, assignment, lecture_feedback
 from backend.data import transcript_generator, vectordb_generator
 from backend.app.services import lecture_summary, topic_search, clarification,extra_questions,assignment_feedback,lecture_review_report, query_handbook, query_grading_doc
