2f92872d0f40108c84d0b7190266dcf1b9bf3d98
kartikey1188 <kartikeysingh428@gmail.com>
2025-03-04 03:47:23 +0530

    Updated YAML

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,22 +1,9 @@
-import os
 import firebase_admin
 from firebase_admin import credentials, firestore
-import json
 
-# Load Firebase credentials from environment variables
-firebase_credentials = {
-    "type": "service_account",
-    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-    #"private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-    "token_uri": "https://oauth2.googleapis.com/token"
-}
-
-# Initialize Firebase only if not already initialized
 if not firebase_admin._apps:
-    cred = credentials.Certificate(firebase_credentials)
+    cred = credentials.Certificate("service-account.json")
     firebase_admin.initialize_app(cred)
 
-# Firestore client
+
 client = firestore.client()
