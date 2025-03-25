7de40b8730728307a23f906409986134c94f47e2
kartikey1188 <kartikeysingh428@gmail.com>
2025-03-25 05:59:57 +0530

    Topic Search Enhanced

--- backend/app/utils/embed_txt.py
+++ backend/app/utils/embed_txt.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from langchain_core.documents import Document
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from app.apis import *
 from flask import current_app as app
